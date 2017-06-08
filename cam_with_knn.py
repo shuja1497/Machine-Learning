b1c78cfae54543246d97baa31eb0634c85c25476
shuja reshi <shujareshi1497@gmail.com>
2017-06-08 22:23:51 +0530

    image recognition using KNN algo and openCV

---
+++ cam_with_knn.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+
+
+rgb = cv2.VideoCapture(0)
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# print facec
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+data_collection = False
+n = 10
+skip_frame = 10
+data = []
+
+
+if not data_collection:
+	addr = ['face_01.npy', 'face_02.npy']
+	data = np.zeros((20, 64, 64, 3))
+
+	data[:10] = np.load(addr[0])
+	data[10:] = np.load(addr[1])
+
+	all_data = data.reshape((20, -1)) # reshapes to (20 12288)
+	#print all_data.shape
+	train_data = np.zeros((20, all_data.shape[1]+1))
+	train_data[:, :-1] = all_data
+	train_data[:, -1] = 1# for face_02
+	train_data[:10, -1] = 2 # for face_01
+
+#print train_data.shape
+#print data.shape
+
+def knn(data, testing, k=5):
+    N = data.shape[0]
+    dist = []
+    
+    for ix in range(N):
+        d = distance(data[ix, :-1], testing)
+        dist.append([d, data[ix, -1]])
+    
+    neighbours = sorted(dist, key=lambda x: x[0])
+    k_neighbours = neighbours[:k]
+    
+    dist_and_labels = np.array(k_neighbours)
+    labels = dist_and_labels[:, -1]
+    
+    freq = np.unique(labels, return_counts=True)
+    return freq[0][freq[1].argmax()]
+
+
+def distance(x1, x2):
+    return np.sqrt(((x1-x2)**2).sum())
+
+
+ix = 0
+flag = False
+while True:
+	_, fr = rgb.read()
+
+	gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+	faces = facec.detectMultiScale(gray, 1.3, 5)
+	ix += 1
+	fac = None
+	
+	for (x,y,w,h) in faces:
+		fc = fr[y:y+h, x:x+w, :]
+		# cv2.imshow('face', fc)
+		# print fc.shape, fr.shape
+		if data_collection:
+			if ix%skip_frame == 0:
+				roi = cv2.resize(fc, (64, 64))
+				data.append(roi)
+			if len(data) >= n:
+				flag = True
+				break
+		else:
+			# Face recognition
+			print "-"*100
+			roi = cv2.resize(fc, (64, 64))
+			print roi.flatten().shape
+			# print train_data.shape
+			out = str(knn(train_data, roi.flatten()))
+			print out
+			labels = {
+				'2.0': 'ABC',
+				'1.0': 'XYZ'
+				}
+
+			cv2.putText(fr, labels[out], (x, y), font,3, (255, 255, 0), 5)
+			# print 'Prediction:', 
+		cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+		fac = fc
+	if flag:
+		break
+	cv2.imshow('rgb', fr)
+	# cv2.imshow('gray', gray)
+	if cv2.waitKey(1) == 27:
+		break
+
+print len(data)
+
+if data_collection:
+	data = np.asarray(data)
+	print data.shape
+	np.save('face_01', data)
+
+cv2.destroyAllWindows()
